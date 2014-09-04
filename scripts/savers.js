d435622bbfba3f4d7bc1e3614470cc830b8f5604
Andy Herbert <andy.herbert@gmail.com>
2014-09-04 18:46:29 +0100

    Refactored the Ansi-related saving routines into saveasansi.js

--- scripts/savers.js
+++ scripts/savers.js
@@ -6,96 +6,6 @@ var Savers = (function () {
     FILETYPE_NONE = 0;
     FILETYPE_ANSI = 1;
 
-    function imageDataToAnsi(imageData) {
-        var inputIndex, end, charCode, fg, bg, bold, blink, currentFg, currentBg, currentBold, currentBlink, attribs, attribIndex, output;
-
-        function ansiColor(binColor) {
-            switch (binColor) {
-            case 1:
-                return 4;
-            case 3:
-                return 6;
-            case 4:
-                return 1;
-            case 6:
-                return 3;
-            default:
-                return binColor;
-            }
-        }
-
-        output = [27, 91, 48, 109];
-        for (inputIndex = 0, end = imageData.height * imageData.width * 3, currentFg = 7, currentBg = 0, currentBold = false, currentBlink = false; inputIndex < end; inputIndex += 3) {
-            attribs = [];
-            charCode = imageData.data[inputIndex];
-            switch (charCode) {
-            case 10:
-                charCode = 9;
-                break;
-            case 13:
-                charCode = 14;
-                break;
-            case 26:
-                charCode = 16;
-                break;
-            case 27:
-                charCode = 17;
-                break;
-            default:
-            }
-            fg = imageData.data[inputIndex + 1];
-            bg = imageData.data[inputIndex + 2];
-            if (fg > 7) {
-                bold = true;
-                fg = fg - 8;
-            } else {
-                bold = false;
-            }
-            if (bg > 7) {
-                blink = true;
-                bg = bg - 8;
-            } else {
-                blink = false;
-            }
-            if ((currentBold && !bold) || (currentBlink && !blink)) {
-                attribs.push([48]);
-                currentFg = 7;
-                currentBg = 0;
-                currentBold = false;
-                currentBlink = false;
-            }
-            if (bold && !currentBold) {
-                attribs.push([49]);
-                currentBold = true;
-            }
-            if (blink && !currentBlink) {
-                attribs.push([53]);
-                currentBlink = true;
-            }
-            if (fg !== currentFg) {
-                attribs.push([51, 48 + ansiColor(fg)]);
-                currentFg = fg;
-            }
-            if (bg !== currentBg) {
-                attribs.push([52, 48 + ansiColor(bg)]);
-                currentBg = bg;
-            }
-            if (attribs.length) {
-                output.push(27, 91);
-                for (attribIndex = 0; attribIndex < attribs.length; ++attribIndex) {
-                    output = output.concat(attribs[attribIndex]);
-                    if (attribIndex !== attribs.length - 1) {
-                        output.push(59);
-                    } else {
-                        output.push(109);
-                    }
-                }
-            }
-            output.push(charCode);
-        }
-        return new Uint8Array(output);
-    }
-
     function imageDataToXBin(imageData, noblink) {
         var bytes, i, j, flags;
         bytes = new Uint8Array((imageData.width * imageData.height * 2) + 11);
@@ -184,7 +94,6 @@ var Savers = (function () {
         return sauce;
     }
 
-
     function saveXBinData(imageData, noblink, title, author, group, filename) {
         var xbin, sauce, combined;
         xbin = imageDataToXBin(imageData, noblink);
@@ -193,16 +102,6 @@ var Savers = (function () {
         combined.set(xbin, 0);
         combined.set(sauce, xbin.length);
         saveFile(combined, "image/x-bin", filename);
-    }
-
-    function saveAnsiData(imageData, noblink, title, author, group, filename) {
-        var ansi, sauce, combined;
-        ansi = imageDataToAnsi(imageData, noblink);
-        sauce = createSauce(DATATYPE_CHARACTER, FILETYPE_ANSI, ansi.length, imageData.width, imageData.height, title, author, group, (noblink ? 1 : 0) + 2 + 16, "IBM VGA");
-        combined = new Uint8Array(ansi.length + sauce.length);
-        combined.set(ansi, 0);
-        combined.set(sauce, ansi.length);
-        saveFile(combined, "image/ansi", filename);
     }
 
     function saveCanvas(canvas, filename) {
@@ -216,7 +115,11 @@ var Savers = (function () {
     return {
         "saveFile": saveFile,
         "saveXBinData": saveXBinData,
-        "saveAnsiData": saveAnsiData,
-        "saveCanvas": saveCanvas
+        "saveCanvas": saveCanvas,
+        "createSauce": createSauce,
+        "DATATYPE_CHARACTER": DATATYPE_CHARACTER,
+        "DATATYPE_XBIN": DATATYPE_XBIN,
+        "FILETYPE_NONE": FILETYPE_NONE,
+        "FILETYPE_ANSI": FILETYPE_ANSI
     };
 }());
--- tools/saveasansi.js
+++ tools/saveasansi.js
@@ -1,6 +1,106 @@
 function saveAsAnsiTool(editor, toolbar, title) {
     "use strict";
 
+    function imageDataToAnsi(imageData) {
+        var inputIndex, end, charCode, fg, bg, bold, blink, currentFg, currentBg, currentBold, currentBlink, attribs, attribIndex, output;
+
+        function ansiColor(binColor) {
+            switch (binColor) {
+            case 1:
+                return 4;
+            case 3:
+                return 6;
+            case 4:
+                return 1;
+            case 6:
+                return 3;
+            default:
+                return binColor;
+            }
+        }
+
+        output = [27, 91, 48, 109];
+        for (inputIndex = 0, end = imageData.height * imageData.width * 3, currentFg = 7, currentBg = 0, currentBold = false, currentBlink = false; inputIndex < end; inputIndex += 3) {
+            attribs = [];
+            charCode = imageData.data[inputIndex];
+            switch (charCode) {
+            case 10:
+                charCode = 9;
+                break;
+            case 13:
+                charCode = 14;
+                break;
+            case 26:
+                charCode = 16;
+                break;
+            case 27:
+                charCode = 17;
+                break;
+            default:
+            }
+            fg = imageData.data[inputIndex + 1];
+            bg = imageData.data[inputIndex + 2];
+            if (fg > 7) {
+                bold = true;
+                fg = fg - 8;
+            } else {
+                bold = false;
+            }
+            if (bg > 7) {
+                blink = true;
+                bg = bg - 8;
+            } else {
+                blink = false;
+            }
+            if ((currentBold && !bold) || (currentBlink && !blink)) {
+                attribs.push([48]);
+                currentFg = 7;
+                currentBg = 0;
+                currentBold = false;
+                currentBlink = false;
+            }
+            if (bold && !currentBold) {
+                attribs.push([49]);
+                currentBold = true;
+            }
+            if (blink && !currentBlink) {
+                attribs.push([53]);
+                currentBlink = true;
+            }
+            if (fg !== currentFg) {
+                attribs.push([51, 48 + ansiColor(fg)]);
+                currentFg = fg;
+            }
+            if (bg !== currentBg) {
+                attribs.push([52, 48 + ansiColor(bg)]);
+                currentBg = bg;
+            }
+            if (attribs.length) {
+                output.push(27, 91);
+                for (attribIndex = 0; attribIndex < attribs.length; ++attribIndex) {
+                    output = output.concat(attribs[attribIndex]);
+                    if (attribIndex !== attribs.length - 1) {
+                        output.push(59);
+                    } else {
+                        output.push(109);
+                    }
+                }
+            }
+            output.push(charCode);
+        }
+        return new Uint8Array(output);
+    }
+
+    function saveAnsiData(imageData, noblink, title, author, group, filename) {
+        var ansi, sauce, combined;
+        ansi = imageDataToAnsi(imageData, noblink);
+        sauce = Savers.createSauce(Savers.DATATYPE_CHARACTER, Savers.FILETYPE_ANSI, ansi.length, imageData.width, imageData.height, title, author, group, (noblink ? 1 : 0) + 2 + 16, "IBM VGA");
+        combined = new Uint8Array(ansi.length + sauce.length);
+        combined.set(ansi, 0);
+        combined.set(sauce, ansi.length);
+        Savers.saveFile(combined, "image/ansi", filename);
+    }
+
     function init() {
         var modal;
 
@@ -20,7 +120,7 @@ function saveAsAnsiTool(editor, toolbar, title) {
             var metadata;
             evt.preventDefault();
             metadata = editor.getMetadata();
-            Savers.saveAnsiData(editor.getImageData(0, 0, editor.getColumns(), editor.getRows()), editor.getBlinkStatus(), metadata.title, metadata.author, metadata.group, title.getText() + ".ans");
+            saveAnsiData(editor.getImageData(0, 0, editor.getColumns(), editor.getRows()), editor.getBlinkStatus(), metadata.title, metadata.author, metadata.group, title.getText() + ".ans");
             dismiss();
         }});
 
