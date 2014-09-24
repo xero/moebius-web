008a88625947cd5452bc84f567393241e695660c
Andy Herbert <andy.herbert@gmail.com>
2014-09-24 12:06:56 +0100

    Show the currently selected tool in the palette tool.

--- tools/palette.js
+++ tools/palette.js
@@ -1,6 +1,18 @@
-function paletteTool(editor, toolbar) {
+function paletteTool(editor) {
     "use strict";
-    var toolCanvas, quickAccess, oldColor, currentColor;
+    var selectionCanvas, paletteCanvas, toolCanvas, quickAccess, oldColor, currentColor;
+
+    function createSelectionCanvas() {
+        var canvas, ctx;
+        canvas = ElementHelper.create("canvas", {"width": 20, "height": 20});
+        ctx = canvas.getContext("2d");
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, 1);
+        ctx.fillRect(0, canvas.height - 1, canvas.width, 1);
+        ctx.fillRect(0, 0, 1, canvas.height);
+        ctx.fillRect(canvas.width - 1, 0, 1, canvas.height);
+        return canvas;
+    }
 
     function mousedown(evt) {
         var pos, color;
@@ -22,7 +34,7 @@ function paletteTool(editor, toolbar) {
 
     function createCanvas() {
         var canvas, ctx, i;
-        canvas = ElementHelper.create("canvas", {"width": 160, "height": 80, "style": {"cursor": "crosshair"}});
+        canvas = ElementHelper.create("canvas", {"width": 160, "height": 80});
         ctx = canvas.getContext("2d");
         for (i = 0; i < 16; i += 1) {
             ctx.fillStyle = editor.codepage.styleRGBA(i, 1);
@@ -33,37 +45,53 @@ function paletteTool(editor, toolbar) {
                 canvas.height / 4
             );
         }
-        canvas.addEventListener("mousedown", mousedown, false);
-        canvas.addEventListener("mousemove", mousemove, false);
         return canvas;
     }
 
-    function redrawCurrentColor(canvas) {
+    function redrawCurrentColor() {
         var ctx;
-        ctx = canvas.getContext("2d");
+        ctx = paletteCanvas.getContext("2d");
         ctx.fillStyle = editor.codepage.styleRGBA(currentColor, 1);
-        ctx.fillRect(0, 0, canvas.width, 40);
+        ctx.fillRect(0, 0, paletteCanvas.width, 40);
+    }
+
+    function redrawTools() {
+        var toolCtx, quickAccessCtx;
+        toolCtx = toolCanvas.getContext("2d");
+        quickAccessCtx = quickAccess.getContext("2d");
+        toolCtx.drawImage(paletteCanvas, 0, 0);
+        quickAccessCtx.drawImage(paletteCanvas, 0, 0);
+        if (currentColor < 8) {
+            toolCtx.drawImage(selectionCanvas, currentColor * 20, 60);
+            quickAccessCtx.drawImage(selectionCanvas, currentColor * 20, 60);
+        } else {
+            toolCtx.drawImage(selectionCanvas, (currentColor - 8) * 20, 40);
+            quickAccessCtx.drawImage(selectionCanvas, (currentColor - 8) * 20, 40);
+        }
     }
 
     function colorChange(color) {
         oldColor = currentColor;
         currentColor = color;
-        redrawCurrentColor(toolCanvas);
-        redrawCurrentColor(quickAccess);
+        redrawCurrentColor();
+        redrawTools();
     }
 
     function paletteChange() {
-        toolCanvas = createCanvas();
-        quickAccess = createCanvas();
-        toolbar.replaceCanvas("palette", toolCanvas);
-        toolbar.replaceQuickAccess("palette", quickAccess);
-        redrawCurrentColor(toolCanvas);
-        redrawCurrentColor(quickAccess);
+        paletteCanvas = createCanvas();
+        redrawCurrentColor();
+        redrawTools();
     }
 
     oldColor = 0;
-    toolCanvas = createCanvas();
-    quickAccess = createCanvas();
+    paletteCanvas = createCanvas();
+    toolCanvas = ElementHelper.create("canvas", {"width": 160, "height": 80, "style": {"cursor": "crosshair"}});
+    quickAccess = ElementHelper.create("canvas", {"width": 160, "height": 80, "style": {"cursor": "crosshair"}});
+    toolCanvas.addEventListener("mousedown", mousedown, false);
+    toolCanvas.addEventListener("mousemove", mousemove, false);
+    quickAccess.addEventListener("mousedown", mousedown, false);
+    quickAccess.addEventListener("mousemove", mousemove, false);
+    selectionCanvas = createSelectionCanvas();
     editor.addColorChangeListener(colorChange);
     editor.addPaletteChangeListener(paletteChange);
 
@@ -73,8 +101,8 @@ function paletteTool(editor, toolbar) {
 
     function onload() {
         currentColor = editor.getCurrentColor();
-        redrawCurrentColor(toolCanvas);
-        redrawCurrentColor(quickAccess);
+        redrawCurrentColor();
+        redrawTools();
     }
 
     function toString() {
